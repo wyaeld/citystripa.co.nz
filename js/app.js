6e4f0ccae7ccb42d05c4d427b63b910cdee3105b
Owen Bannister <ob2640@gmail.com>
2015-02-07 16:01:29 +1300

    Completed contact form

--- js/app.js
+++ js/app.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
     $('.carousel').carousel({
         interval: 3000
     })
+ 
+    // Handle thank you
+  var url = window.location.href;
+  if(url.indexOf('?thank-you') != -1) {
+    $('#thankyou').removeClass('hidden');
+  }
+        
 });
+
+
